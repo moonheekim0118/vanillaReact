77ab864f68df17834c80ea14ddf46be1b0169a8b
moonee <uuuu118@naver.com>
2020-12-28 14:40:28 +0900

    add : about페이지 내용 추가

--- src/pages/about.ts
+++ src/pages/about.ts
@@ -1,9 +1,14 @@
 import { changeTitle } from "../util/util";
 
 const About = () => {
-    const container = document.createElement("div");
+    const container = document.createElement("side");
     container.className = "container";
-    container.innerText = "about page";
+    const description = `
+    <h2>a Swiftie made this site just for fun🤩</h2>
+    <p>I use <a href="https://taylor.rest/">taylor rest API</a> for serving her photos and quotes</p>
+    `;
+
+    container.innerHTML = description;
     changeTitle("Taylor TODO | about");
 
     return container;
